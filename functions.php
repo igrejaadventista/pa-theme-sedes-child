ce5f71dc9accf59b3a7630e2a8a1d0aca471abfd
Eli Mendonça <contato@elimendonca.com>
2020-10-13 22:22:03 -0300

    - Inclui os arquivos do slider da front-page - Cria o CPT Slider Home - Cria campos ACF p/ o Slider Home

--- functions.php
+++ functions.php
@@ -1,12 +1,4 @@
 <?php
 require_once (dirname(__FILE__) . '/classes/controllers/PA_CPT_Projects.class.php');
+require_once (dirname(__FILE__) . '/classes/controllers/PA_CPT_SliderHome.class.php');
 require_once (dirname(__FILE__) . '/classes/controllers/PA_Enqueue_Files.class.php');
-
-// function filter_print() {
-// 	global $wp_filter;
-// 	echo "<pre>";
-// 	print_r( $wp_filter['admin_bar_menu'] );
-// 	echo "</pre>"; 
-//     die();
-// }
-// add_action( 'admin_init', 'filter_print' );
