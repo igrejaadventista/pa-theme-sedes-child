29f0ef607c8b66b94fc3b668beece6c9ff6904c7
Eli Mendonça <contato@elimendonca.com>
2020-10-15 08:33:10 -0300

    Adiciona chamada que cria a página das opções do tema

--- functions.php
+++ functions.php
@@ -2,3 +2,11 @@
 require_once (dirname(__FILE__) . '/classes/controllers/PA_CPT_Projects.class.php');
 require_once (dirname(__FILE__) . '/classes/controllers/PA_CPT_SliderHome.class.php');
 require_once (dirname(__FILE__) . '/classes/controllers/PA_Enqueue_Files.class.php');
+
+if( function_exists('acf_add_options_page') ) {	
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'PA - Theme Settings',
+		'menu_title'	=> 'PA - Theme Settings',
+		'parent_slug'	=> 'themes.php',
+	));	
+}
