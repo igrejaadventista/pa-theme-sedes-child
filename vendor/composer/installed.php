a58f1f4b9b9d87c908b638bc5afcb07ecc4fd91e
Eli Mendonça <internetdsa@gmail.com>
2021-10-06 11:20:48 -0300

    Atualiza composer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,33 +1,32 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => 'dev-master',
-    'version' => 'dev-master',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '806ece2c647f2e726a47b87a115000ed423a34ad',
+        'name' => '__root__',
+        'dev' => true,
     ),
-    'reference' => '26ab4940eb70a2a47f1e2b3ac31dbd0f0bd01492',
-    'name' => '__root__',
-  ),
-  'versions' => 
-  array (
-    '__root__' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '26ab4940eb70a2a47f1e2b3ac31dbd0f0bd01492',
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '806ece2c647f2e726a47b87a115000ed423a34ad',
+            'dev_requirement' => false,
+        ),
+        'wordplate/acf' => array(
+            'pretty_version' => '9.1.0',
+            'version' => '9.1.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wordplate/acf',
+            'aliases' => array(),
+            'reference' => 'c2a8c48a1580b2a5e9978d3350ff8c3d4df3e115',
+            'dev_requirement' => false,
+        ),
     ),
-    'wordplate/acf' => 
-    array (
-      'pretty_version' => '9.1.0',
-      'version' => '9.1.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'c2a8c48a1580b2a5e9978d3350ff8c3d4df3e115',
-    ),
-  ),
 );
