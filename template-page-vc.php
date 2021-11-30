7da205c8e161ab556cd4b60cb598fda90ab9a76a
Eli Mendonça <contato@elimendonca.com>
2021-11-30 00:21:22 -0300

    Inclui o template do visual composer

---
+++ template-page-vc.php
@@ -0,0 +1,36 @@
+<?php
+
+	/* Template name: Página - Visual Composer */
+
+	get_header("visual-composer"); 
+	if(have_posts())
+		the_post();
+?>
+<!-- *************************** -->
+<!-- ********* Content ********* -->
+<!-- *************************** -->
+<div class="container">
+	<section class="row">
+		<article class="col-md-12 entry-content">
+			<?php the_content(); ?>
+		</article>
+	</section>
+</div>
+
+<?php if ( comments_open() ) { ?>
+<section class="comments">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<?php comments_template(); ?>
+			</div>
+		</div>
+	</div>
+</section>
+<?php } ?>
+
+<!-- *************************** -->
+<!-- ******* End Content ******* -->
+<!-- *************************** -->
+
+<?php get_footer(); ?>
