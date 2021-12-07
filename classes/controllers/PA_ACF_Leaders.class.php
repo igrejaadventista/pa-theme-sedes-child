b73f07d593ca061dd6009b0a993335596d34d473
mizael isaltino <mizael.isaltino@gmail.com>
2021-12-07 10:14:07 -0300

    Ajustando página de lideres.

--- classes/controllers/PA_ACF_Leaders.class.php
+++ classes/controllers/PA_ACF_Leaders.class.php
@@ -3,13 +3,17 @@
 use WordPlate\Acf\Fields\PostObject;
 use WordPlate\Acf\Location;
 
-class PaAcfLeaders {
+class PaAcfLeaders
+{
 
-    public function __construct(){
+    public function __construct()
+    {
         add_action('init', [$this, 'createACFFields']);
+        add_action('init', [$this, 'hideEditor']);
     }
 
-    function createACFFields(){
+    function createACFFields()
+    {
         register_extended_field_group([
             'title' => 'Leader-feature',
             'style' => 'default',
@@ -24,22 +28,26 @@ class PaAcfLeaders {
             ]
         ]);
     }
- 
-    function hideEditor() {
-    
-        if ( isset($_GET['post']) || isset($_POST['post_ID']) ) {
-            $post_id = $_GET['post'] ? $_GET['post'] : $_POST['post_ID'] ;
+
+    function hideEditor()
+    {
+        if (!is_admin()) {
+            return;
         }
-        
-        if( !isset( $post_id ) ) return;
-    
+
+        if (isset($_GET['post']) || isset($_POST['post_ID'])) {
+            $post_id = $_GET['post'] ? $_GET['post'] : $_POST['post_ID'];
+        }
+
+        if (!isset($post_id)) return;
+
         $template_file = get_post_meta($post_id, '_wp_page_template', true);
-        
-        if($template_file == 'page-lideres.php'){ // edit the template name
+
+        if ($template_file == 'page-lideres.php') { // edit the template name
+
             remove_post_type_support('page', 'editor');
         }
     }
-
 }
 
 $PaAcfLeaders = new PaAcfLeaders();
--- page-lideres.php
+++ page-lideres.php
@@ -1,13 +1,13 @@
-<?php 
+<?php
 
-    /* Template name: Page - Líder */
-    
-	get_header();
+/* Template name: Page - Líder */
 
-	if(have_posts())
-		the_post();
+get_header();
 
-	require(get_template_directory() . '/components/parent/header.php');
+if (have_posts())
+	the_post();
+
+require(get_template_directory() . '/components/parent/header.php');
 
 
 ?>
@@ -15,42 +15,39 @@
 	<div class="container">
 		<div class="pa-lideres-destaque row d-flex justify-content-center">
 			<?php
-			$lider_id = array();
-            $lideres = get_field('lideres_destaques');
-
+			$lideres = get_field('lideres_destaques');
 			pa_lideres_destaque($lideres);
-            
-			wp_reset_postdata(); 
+			wp_reset_postdata();
 			?>
-        </div>
-       
+		</div>
+
 		<div class="pa-lideres-geral row d-flex justify-content-center pt-5">
 			<?php
-				$args = array(
-					'numberposts'	=> -1,
-					'post_type'		=> 'lideres',
-					'post_status'	=> 'publish',
-					'post__not_in' => $lider_id
-				);
-			
-				$query = new WP_Query($args);
-				
-				while ($query->have_posts()) : $query->the_post();
+			$args = array(
+				'numberposts'	=> -1,
+				'post_type'		=> 'lideres',
+				'post_status'	=> 'publish',
+				'post__not_in' => $lideres
+			);
+
+			$query = new WP_Query($args);
+
+			while ($query->have_posts()) : $query->the_post();
 			?>
-			<div class="pa-lider-geral col col-xl-3 mb-5 text-center">
-				<a href="<?php echo get_permalink(); ?>">
-					<?php the_post_thumbnail( array(200, 200), array( 'class' => 'pa-lider-thumb rounded-circle' ) ); ?>
-					<p class="mt-4 mb-0 fw-bold"><?php the_title(); ?></p>
-					<p class="mb-0 font-italic"><?php the_field('lider_cargo', get_the_ID()); ?></p>
-					<p class="pa-link-perfil mb-0 fw-bold invisible">Ver perfil</p>
-				</a>
-			</div>
-			<?php 
-				endwhile; 
-				wp_reset_postdata(); 
+				<div class="pa-lider-geral col col-xl-3 mb-5 text-center">
+					<a href="<?= get_permalink(); ?>">
+						<?= the_post_thumbnail(array(200, 200), array('class' => 'pa-lider-thumb rounded-circle')); ?>
+						<p class="mt-4 mb-0 fw-bold"><?= the_title(); ?></p>
+						<p class="mb-0 font-italic"><?= the_field('lider_cargo', get_the_ID()); ?></p>
+						<p class="pa-link-perfil mb-0 fw-bold invisible">Ver perfil</p>
+					</a>
+				</div>
+			<?php
+			endwhile;
+			wp_reset_postdata();
 			?>
 		</div>
 	</div>
 </section>
 
-<?php get_footer();?>
+<?php get_footer(); ?>
