ba2b06a5ce6698a266eb0b66960e2d649c88e73e
isaltino <mizael.isaltino@gmail.com>
2022-09-16 09:15:34 -0300

    Adicionando na página archive de distritos o número de igrejas e o nome do pastor.

--- taxonomy-xtt-pa-region.php
+++ taxonomy-xtt-pa-region.php
@@ -1,11 +1,7 @@
-<?php
-get_header();
-
-$args = array('post_type' => 'districts');
-
-$the_query = new WP_Query($args);
-
+<?php 
+	get_header();
 ?>
+
 <section class="pa-header py-3">
     <header class="container">
         <div class="row">
@@ -26,16 +22,17 @@ $the_query = new WP_Query($args);
                 <div class="pa-blog-itens my-5">
                     <?php
 
-                    if ($the_query->have_posts()) :
-                        while ($the_query->have_posts()) : $the_query->the_post();
+                    if (have_posts()) :
+                        while (have_posts()) : the_post();
                     ?>
                             <div class="pa-blog-item mb-4 mb-md-4 border-0">
                                 <a href="<?= get_the_permalink() ?>" title="<?= get_the_title() ?>">
                                     <div class="row">
                                         <div class="col">
                                             <div class="card-body ps-4 pe-0 py-4 border-start border-5 pa-border">
-                                                <span class="pa-tag text-uppercase d-none d-xl-table-cell rounded">Distrito</span>
+                                                <span class="pa-tag text-uppercase d-none d-xl-table-cell rounded"><?= __('Distrito', 'iasd') ?></span>
                                                 <h3 class="fw-bold h2 mt-xl-2 pa-truncate-4"><?= get_the_title() ?></h3>
+                                                <label><i class="fas fa-church pe-2"></i><?= count(get_field('churches')) ?><?= get_field('shepherds') ? ' - ' . get_field('shepherds')[0]['shepherd_nome']: ''?></label>
                                             </div>
                                         </div>
                                     </div>
@@ -48,6 +45,9 @@ $the_query = new WP_Query($args);
                     wp_reset_postdata();
                     ?>
                 </div>
+                <div class="pa-pg-numbers row">
+					<?php PaThemeHelpers::pageNumbers(); ?>
+				</div>
             </section>
 
         </div>
